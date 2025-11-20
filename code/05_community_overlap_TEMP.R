d09014b37a25687bfadbdce437ab767c43150af2
MarikaFroio <marika.froio@studio.unibo.it>
2025-11-20 17:09:04 +0100

    Update 05_community_overlap_TEMP.R

--- code/05_community_overlap_TEMP.R
+++ code/05_community_overlap_TEMP.R
@@ -30,6 +30,38 @@ densityPlot(macaque$Timecirc)
 overlapPlot(tigertime, macaquetime)
 #we can now compare the temporal relationship. we can create a loop to repeat the same action for every species
 
+#unique is a function that selects each species
+#select the species
+#assign the time
+#create the graph
+
+
+
+
+
+#multiframe
+species_list <- unique(kerinci$Sps)
+
+# Set up a multiframe: choose layout based on number of species
+n <- length(species_list)
+rows <- ceiling(sqrt(n))
+cols <- ceiling(n / rows)
+
+par(mfrow = c(rows, cols))   # create multiframe layout
+
+# Loop to plot each species
+for(sp in species_list){
+  
+  temp <- kerinci[kerinci$Sps == sp, ]
+  temp_time <- temp$Timecirc
+  
+  densityPlot(temp_time,
+              main = paste("Density plot:", sp))
+}
+
+# Reset plotting layout
+par(mfrow = c(1,1))
+
 
 
 timetiger<tiger$circ
